07c27a570fe192496029ab6b9d18d96f87d0f166
hyunnn0105 <asha010519@gmail.com>
2023-08-27 21:58:32 +0900

    part2_주문과 할인도메인 실행과 테스트 to part3_새로운할인정책개발

---
+++ src/test/java/hello/core/order/OrderAppTest.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+
+    @Test
+    void creatOrder(){
+
+        Long memberId = 1L; // long 생성하면 null 설정 불가능
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.joinMember(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        
+    }
+
+
+}
