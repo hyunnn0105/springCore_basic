ac6c470ac485fd4c73db785639a9abc807910116
hyunnn0105 <asha010519@gmail.com>
2023-09-10 15:17:07 +0900

    section3_새로운_할인정책의_문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixdiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,16 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixdiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixdiscountPolicy();
+    
+    // 변동금액으로 변경하기 ==> OCP, DIP 위반
+    // 추상 인터페이스 + 구현 클래스에도 의존하고 있음 (DIP 위반)
+    // interface만 변경하도록 구조를 변경해야 한다.
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // ==> interface에만 의존하는 코드로 변경 but 구현체가 없음
+    // 해결방안 : 클라이언트의 OrderServiceImpl에  discountPolicy의 구현 객체를 대신 생성하고 주입해야 한다.
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberID, String itemName, int itemPrice) {
