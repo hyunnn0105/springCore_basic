b1dd2b590460f65ecdfdf44d7c49dab665df113b
hyunnn0105 <asha010519@gmail.com>
2023-09-16 17:41:39 +0900

    section4_스프링_빈_조회_기본

--- src/test/java/hello/core/beenFind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beenFind/ApplicationContextBasicFindTest.java
@@ -3,13 +3,13 @@ package hello.core.beenFind;
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationContextBasicFindTest {
 
@@ -47,7 +47,7 @@ class ApplicationContextBasicFindTest {
     @Test
     @DisplayName("빈 이름으로 조회X")
     void findBeanByNamesX(){
-        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class
+        assertThrows(NoSuchBeanDefinitionException.class
                 ,() -> ac.getBean("xxxx", MemberService.class));
     }
 
